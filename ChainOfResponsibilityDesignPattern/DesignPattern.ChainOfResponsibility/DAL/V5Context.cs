ebb6a8c0b7a01d8909b56f1e1edc74e36da27c05
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-11-08 21:18:10 +0300

    B1_V5_Migration_Islemleri

---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/V5Context.cs
@@ -0,0 +1,18 @@
+﻿
+//using Microsoft.EntityFrameworkCore;
+
+//namespace DesignPattern.ChainOfResponsibility.DAL
+//{
+//    public class V5Context : DbContext
+//    {
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            optionsBuilder.UseSqlServer("server=DESKTOP-K88T80S\\SQLEXPRESS;initial catalog=DesignPattern1;integrated security=true;");
+//        }
+
+//        public DbSet<V5CustomerProcess> V5CustomerProcesses { get; set; }
+
+//    }
+
+//}
---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/V5CustomerProcess.cs
@@ -0,0 +1,17 @@
+﻿
+//using System.ComponentModel.DataAnnotations;
+
+//namespace DesignPattern.ChainOfResponsibility.DAL
+//{
+//    public class V5CustomerProcess
+//    {
+//        [Key]
+//        public int CustomerProcessID { get; set; }
+//        public string Name { get; set; }
+//        public string Amount { get; set; }
+//        public string EmployeeName { get; set; }
+//        public string Description { get; set; }
+
+
+//    }
+//}
